b78fb5c7a312b01b89614f50d2036f919e9c17a8
Ducz01 <tranthinhduc01@web.de>
2022-12-01 20:04:00 +0100

    github login done

--- deinplugin/deinplugin/urls.py
+++ deinplugin/deinplugin/urls.py
@@ -21,7 +21,7 @@ from main import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', views.login, name='login'),
-    path('logout', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('social-auth/', include('social_django.urls', namespace='social')),
     path("", views.home, name="home"),
 ]
