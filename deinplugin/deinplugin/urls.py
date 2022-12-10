380c45a4e0bbfa74c84e044ad2d06c4b3c9c9b24
Ducz01 <tranthinhduc01@web.de>
2022-12-10 19:04:59 +0100

    update api url

--- deinplugin/deinplugin/urls.py
+++ deinplugin/deinplugin/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
    # path('login/', views.login, name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('social-auth/', include('social_django.urls', namespace='social')),
-    path('api/', include('main.urls')),
+    path('main/', include('main.urls')),
     path("api-auth/", include("rest_framework.urls")),
 ]
