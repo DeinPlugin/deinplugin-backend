3619a38ccf8b3f89d4795727ff4385c74fbea511
Ducz01 <tranthinhduc01@web.de>
2022-12-18 16:30:02 +0100

    fetch yaml from default branch

--- deinplugin/main/utils.py
+++ deinplugin/main/utils.py
@@ -4,9 +4,9 @@ def get_plugin_info(github_url):
     splitted_url = github_url.split('/')
     repository_name = splitted_url[-1]
     user_name = splitted_url[-2]
-    result = requests.get('https://raw.githubusercontent.com/' + user_name + '/' + repository_name + '/master/deinplugin.yaml')
-    print('https://raw.githubusercontent.com/' + user_name + '/' + repository_name + '/master/deinplugin.yaml')
-    
+    default_branch = requests.get('https://api.github.com/repos/' + user_name + '/' + repository_name).json()['default_branch']
+    result = requests.get(f'https://raw.githubusercontent.com/{user_name}/{repository_name}/{default_branch}/deinplugin.yaml')
+   
     # parse the file
     if result.status_code == 200:
         # bytes to string
